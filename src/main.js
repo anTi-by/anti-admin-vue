cc19bf2016812808ea0b64f0fcce43109af07f81
anTi <512451756@qq.com>
2020-03-16 17:32:46 +0800

    mock configure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// main.js 开启mock 服务
+import { mockXHR } from "../mock";
+if (process.env.NODE_ENV === "development") {
+  mockXHR();
+}
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,37 @@
+"use strict";
+const path = require("path");
+
+const name = "anTi后台管理"; // page title
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+const port = 9528; // dev port
+
 module.exports = {
-  lintOnSave: false
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  lintOnSave: false,
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    before: require("./mock/mock-server.js")
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        "@": resolve("src")
+      }
+    }
+  }
 };
